e13a7d66e940b886458354946c814332ce4c0fa2
temujin .s <temujin.s@steppelink.mn>
2024-12-04 19:26:18 +0800

    fetch api

--- src/components/Index/Section7.tsx
+++ src/components/Index/Section7.tsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Section9 = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch(
+      "https://data.num.edu.mn/dataset/e92f508e-6a2d-46f6-b078-a3c99a6c7263/resource/9fccf580-281a-402a-922e-7ee6da187396/download/-2023-autumn.json",
+      {
+        mode: "no-cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response: any) => {
+        setData(response.json());
+      })
+      .then((data) => {
+        console.log("Fetched Data:", data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  console.log("data", data);
+
   return (
     <div
       className="relative mx-auto container py-[120px] sm:flex block justify-center space-x-40"
@@ -8,7 +33,7 @@ const Section9 = () => {
     >
       <div className="">
         <h1 className="font-semibold sm:text-black text-indigo-500 sm:text-2xl text-lg sm:text-start text-center">
-        Шинээр элсэгч
+          Шинээр элсэгч
         </h1>
         <h1 className="sm:text-[#0053AD] text-black sm:text-[49px] text-[32px] font-bold sm:text-start text-center">
           Ботгоны адал явдал
@@ -19,10 +44,12 @@ const Section9 = () => {
           className="sm:hidden w-[240px] my-10 mx-auto block"
         />
         <p className="text-sm text-justify sm:mx-0 mx-auto sm:w-[540px] w-[342px] ">
-        МУИС-ийн 13 дахь хичээлийн долоо хоног үргэлжилж байна оюутан 
-        залуусаа ! 
-        Одоо бидэнд ахин 3 долоо хоног үлджээ
+          МУИС-ийн 13 дахь хичээлийн долоо хоног үргэлжилж байна оюутан залуусаа
+          ! Одоо бидэнд ахин 3 долоо хоног үлджээ
         </p>
+        {/* {data?.map((item) => (
+          <div></div>
+        ))} */}
       </div>
       <div className="">
         <img src="/botgo.jpg" alt="botgo" className="sm:block hidden" />
